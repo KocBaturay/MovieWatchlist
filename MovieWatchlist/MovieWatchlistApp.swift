e9e5cfe9620c0ab3f37600a34a7951156b195cf6
Baturay Koc <baturaykocc@gmail.com>
2026-08-16 15:29:11 +0200

    Initial Commit

---
+++ MovieWatchlist/MovieWatchlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieWatchlistApp.swift
+//  MovieWatchlist
+//
+//  Created by Baturay Koc on 16.08.26.
+//
+
+import SwiftUI
+
+@main
+struct MovieWatchlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
